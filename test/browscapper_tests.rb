6c2f8050209c19d5b955fada016aedfb0cd97180
J Smith <dark.panda@gmail.com>
2014-12-02 12:15:02 -0500

    Add a basic `browscap.ini` file for consistent testing.

--- test/browscapper_tests.rb
+++ test/browscapper_tests.rb
@@ -15,8 +15,8 @@ class BrowscapperTest < MiniTest::Unit::TestCase
     :cookies => true,
     :crawler => false,
     :css_version => 3,
-    :device_name => "unknown",
-    :device_maker => "unknown",
+    :device_name => "Macintosh",
+    :device_maker => "Apple Inc",
     :frames => true,
     :iframes => true,
     :java_applets => true,
@@ -26,17 +26,17 @@ class BrowscapperTest < MiniTest::Unit::TestCase
     :mobile_device => false,
     :parent => "Firefox 3.6",
     :platform => "MacOSX",
-    :platform_version => "unknown",
+    :platform_version => 10.6,
     :rendering_engine_name => "Gecko",
-    :rendering_engine_version => "unknown",
+    :rendering_engine_version => "1.9.2",
     :rendering_engine_description => "For Firefox, Camino, K-Meleon, SeaMonkey, Netscape, and other Gecko-based browsers.",
     :syndication_reader => false,
     :tables => true,
     :tablet => false,
-    :user_agent => "Mozilla/5.0 (Macintosh; *; *Mac OS X*; *; rv:1.9.2*) Gecko/* Firefox/3.6*",
+    :user_agent => "Mozilla/5.0 (*Mac OS X 10.6*) Gecko/* Firefox/3.6*",
     :user_agent_id => nil,
     :vbscript => false,
-    :version => "3.6",
+    :version => 3.6,
     :win16 => false,
     :win32 => false,
     :win64 => false,
@@ -45,7 +45,7 @@ class BrowscapperTest < MiniTest::Unit::TestCase
   FIREFOX_ON_OSX.pattern = /^mozilla\/5\.0 \(macintosh; .*; .*mac os x.*; .*; rv:1\.9\.2.*\) gecko\/.* firefox\/3\.6.*$/
 
   def test_browscap_ini
-    Browscapper.load(File.join('vendor', 'browscap.dump'))
+    Browscapper.load(File.join('vendor', 'browscap.ini'))
     match = Browscapper.match('Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8')
 
     FIREFOX_ON_OSX.each do |k, v|
